249e65e576ed00b11e5fd9c7e55f9075d7f0da04
EcheCostabel <exequielcostabel@gmail.com>
2023-05-10 15:39:08 -0300

    adding Drawer Navigator

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { AppRegistry, Platform } from "react-native";
+import { SideBar } from './src/navigator/SideBar';
 
 
 
 
 export default function App() {
+
+  AppRegistry.registerComponent('App', () => App);
   return (
     <NavigationContainer>
 
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideBar />
 
     
     </NavigationContainer>
