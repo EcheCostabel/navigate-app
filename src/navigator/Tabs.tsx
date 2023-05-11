d7bc1ee6ebdabe2976b185fcf28f229a52db2801
EcheCostabel <exequielcostabel@gmail.com>
2023-05-11 19:21:36 -0300

    styling Tabs

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,22 +1,35 @@
 import React from 'react'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { StackNavigator } from './StackNavigator';
+import {colors} from  '../theme/appTheme'
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export const Tabs = (  ) => {
   return (
     <Tab.Navigator 
-   
+      sceneContainerStyle={{
+        backgroundColor: 'white' //este es el fondo de lo que esta en cada TabScreen
+      }}
+      screenOptions={{  //esto es lo que esta en los botones de abajo
+        tabBarActiveTintColor: colors.primary,
+        tabBarStyle: { 
+          borderTopColor: colors.primary, 
+          borderTopWidth: 0,
+          elevation: 0
+        }
+
+      }}
+      
     
     >
       
     
-      <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-      <Tab.Screen name="StackNavigator" component={StackNavigator} />
+      <Tab.Screen name="Tab1Screen" options={{title: 'Tab1'}} component={Tab1Screen} />
+      <Tab.Screen name="Tab2Screen" options={{title: 'Tab2'}} component={Tab2Screen} />
+      <Tab.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
     </Tab.Navigator>
   );
 }
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,6 +1,12 @@
 import { StyleSheet } from 'react-native';
 
 
+export const colors = {
+
+    primary: '#5856D6',
+    
+}
+
 
 
 
