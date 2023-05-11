9e06e0255d0b75267418ac45bd2c52ba4c64d6aa
EcheCostabel <exequielcostabel@gmail.com>
2023-05-11 15:36:17 -0300

    start

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 
 export const Tab1Screen = () => {
+
+  useEffect(() => {
+    console.log('Tab1Screem')
+  }, [])
+
+
+
   return (
     <View>
         <Text>Tab1Screen</Text>
--- src/screens/Tab2Screen.tsx
+++ src/screens/Tab2Screen.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
 
 
 export const Tab2Screen = () => {
+
+  useEffect(() => {
+    console.log('Tab1Screem')
+  }, [])
+
   return (
     <View>
         <Text>Tab2Screen</Text>
