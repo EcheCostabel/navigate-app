7f4b368c1ab7a1b2ff2e221cdb45287221c1190d
EcheCostabel <exequielcostabel@gmail.com>
2023-05-11 21:30:57 -0300

    instaling Icons

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect } from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { styles } from '../theme/appTheme';
+
 
 export const Tab1Screen = () => {
 
@@ -10,8 +14,14 @@ export const Tab1Screen = () => {
 
 
   return (
-    <View>
-        <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+        <Text style={styles.title}>Icons</Text>
+        
+     <Text>
+
+        <Icon name="airplane-outline" size={50} color="#900" />
+     </Text>
+
     </View>
   )
 }
